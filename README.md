1b9766dbe58087c106119aa9686b815cca4ae0de
Keith Taylor <taylor.km61@icloud.com>
2024-05-21 22:19:33 +0100

    Transform get response

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
 
-| Tag   | Note           | Topic         |
-| ----- | -------------- | ------------- |
-| 0.1.0 | Initial commit | scaffold      |
-| 0.2.0 | Preparation    | Project start |
+| Tag   | Note                   | Topic         |
+| ----- | ---------------------- | ------------- |
+| 0.1.0 | Initial commit         | scaffold      |
+| 0.2.0 | Preparation            | Project start |
+| 0.2.1 | First Post             | Post data     |
+| 0.2.2 | First Get              | Get data      |
+| 0.2.3 | Transform get response | RxJs          |
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.fetchPosts();
-   }
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -39,6 +40,15 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http.get('https://ng-max-guide-428f9-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key as keyof typeof responseData], id: key })
+          }
+        }
+        return postsArray;
+      }))
       .subscribe(posts => {
         console.log(posts);
       });
